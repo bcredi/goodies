d29bcd94ee3204d4d7ea1f9cbc09334c6fb32df2
Mateus <mateus.rstrey@gmail.com>
2020-09-23 18:05:52 -0300

    change test config [2]

--- lib/goodies/oban/v1/appsignal_telemetry_logger.ex
+++ lib/goodies/oban/v1/appsignal_telemetry_logger.ex
@@ -21,26 +21,28 @@ defmodule Goodies.Oban.V1.AppsignalTelemetryLogger do
   """
   alias Appsignal.{Error, Transaction}
 
-  @transaction Application.compile_env(:goodies, :appsignal_transaction_module)
-
   def handle_event([:oban, event], measurement, meta, _) when event in [:success, :failure] do
     transaction = record_event(measurement, meta)
 
     if event == :failure && meta.attempt >= meta.max_attempts do
       {reason, message, stack} = normalize_error(meta)
-      @transaction.set_error(transaction, reason, message, stack)
+      transaction_module().set_error(transaction, reason, message, stack)
     end
 
-    @transaction.complete(transaction)
+    transaction_module().complete(transaction)
+  end
+
+  defp transaction_module do
+    Application.fetch_env!(:goodies, :appsignal_transaction_module)
   end
 
   defp record_event(measurement, meta) do
     metadata = %{"id" => meta.id, "queue" => meta.queue, "attempt" => meta.attempt}
 
-    transaction = @transaction.start(Transaction.generate_id(), :background_job)
+    transaction = transaction_module().start(Transaction.generate_id(), :background_job)
 
     transaction
-    |> @transaction.set_action("#{meta.worker}#perform")
+    |> transaction_module().set_action("#{meta.worker}#perform")
     |> Transaction.set_meta_data(metadata)
     |> Transaction.set_sample_data("params", meta.args)
     |> Transaction.record_event("worker.perform", "", "", measurement.duration, 0)
