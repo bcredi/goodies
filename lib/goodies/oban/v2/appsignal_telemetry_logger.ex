61cda253617aab951f154cfa50b6caa205a47918
Mateus Rambo <mateus.rstrey@gmail.com>
2021-06-08 15:22:02 -0300

    check dependency presence before loading modules

--- lib/goodies/oban/v2/appsignal_telemetry_logger.ex
+++ lib/goodies/oban/v2/appsignal_telemetry_logger.ex
@@ -1,59 +1,61 @@
-defmodule Goodies.Oban.V2.AppsignalTelemetryLogger do
-  @moduledoc """
-  This module logs Oban v2 Telemetry events on Appsignal.
+if Code.ensure_loaded?(Appsignal) do
+  defmodule Goodies.Oban.V2.AppsignalTelemetryLogger do
+    @moduledoc """
+    This module logs Oban v2 Telemetry events on Appsignal.
 
-  To use it, declare on `MyApp.Application.start/1` of the app that is using Oban:
-  ```elixir
-    :telemetry.attach(
-      "oban-job-stop",
-      [:oban, :job, :stop],
-      &Goodies.Oban.V2.AppsignalTelemetryLogger.handle_event/4,
-      nil
-    )
+    To use it, declare on `MyApp.Application.start/1` of the app that is using Oban:
+    ```elixir
+      :telemetry.attach(
+        "oban-job-stop",
+        [:oban, :job, :stop],
+        &Goodies.Oban.V2.AppsignalTelemetryLogger.handle_event/4,
+        nil
+      )
 
-    :telemetry.attach(
-      "oban-job-exception",
-      [:oban, :job, :exception],
-      &Goodies.Oban.V2.AppsignalTelemetryLogger.handle_event/4,
-      nil
-    )
-  ```
-  """
-  alias Appsignal.{Error, Transaction}
+      :telemetry.attach(
+        "oban-job-exception",
+        [:oban, :job, :exception],
+        &Goodies.Oban.V2.AppsignalTelemetryLogger.handle_event/4,
+        nil
+      )
+    ```
+    """
+    alias Appsignal.{Error, Transaction}
 
-  def handle_event([:oban, :job, event], measurement, meta, _)
-      when event in [:stop, :exception] do
-    transaction = record_event(measurement, meta)
+    def handle_event([:oban, :job, event], measurement, meta, _)
+        when event in [:stop, :exception] do
+      transaction = record_event(measurement, meta)
 
-    if event == :exception && meta.attempt >= meta.max_attempts && meta.kind != :exit do
-      {reason, message, stack} = normalize_error(meta)
-      Transaction.set_error(transaction, reason, message, stack)
+      if event == :exception && meta.attempt >= meta.max_attempts && meta.kind != :exit do
+        {reason, message, stack} = normalize_error(meta)
+        Transaction.set_error(transaction, reason, message, stack)
+      end
+
+      Transaction.complete(transaction)
     end
 
-    Transaction.complete(transaction)
-  end
+    defp record_event(measurement, meta) do
+      metadata = %{"id" => meta.id, "queue" => meta.queue, "attempt" => meta.attempt}
 
-  defp record_event(measurement, meta) do
-    metadata = %{"id" => meta.id, "queue" => meta.queue, "attempt" => meta.attempt}
+      transaction = Transaction.start(Transaction.generate_id(), :background_job)
 
-    transaction = Transaction.start(Transaction.generate_id(), :background_job)
+      transaction
+      |> Transaction.set_action("#{meta.worker}#perform")
+      |> Transaction.set_meta_data(metadata)
+      |> Transaction.set_sample_data("params", meta.args)
+      |> Transaction.record_event("worker.perform", "", "", measurement.duration, 0)
+      |> Transaction.finish()
 
-    transaction
-    |> Transaction.set_action("#{meta.worker}#perform")
-    |> Transaction.set_meta_data(metadata)
-    |> Transaction.set_sample_data("params", meta.args)
-    |> Transaction.record_event("worker.perform", "", "", measurement.duration, 0)
-    |> Transaction.finish()
+      transaction
+    end
 
-    transaction
-  end
+    defp normalize_error(%{error: error, stacktrace: stack}) do
+      {reason, message} = Error.metadata(error)
+      {stringify(reason), stringify(message), stack}
+    end
 
-  defp normalize_error(%{error: error, stacktrace: stack}) do
-    {reason, message} = Error.metadata(error)
-    {stringify(reason), stringify(message), stack}
-  end
-
-  defp stringify(value) do
-    if is_binary(value), do: value, else: inspect(value, limit: :infinity)
+    defp stringify(value) do
+      if is_binary(value), do: value, else: inspect(value, limit: :infinity)
+    end
   end
 end
--- lib/goodies/tesla/middleware/request_id_forwarder.ex
+++ lib/goodies/tesla/middleware/request_id_forwarder.ex
@@ -1,24 +1,26 @@
-defmodule Goodies.Tesla.Middleware.RequestIdForwarder do
-  @moduledoc """
-  Tesla middleware to forward *x-request-id* header to others services.
+if Code.ensure_loaded?(Tesla) do
+  defmodule Goodies.Tesla.Middleware.RequestIdForwarder do
+    @moduledoc """
+    Tesla middleware to forward *x-request-id* header to others services.
 
-  Assigns `Logger.metadata()[:request_id]` to *x-request-id* header.
-  """
+    Assigns `Logger.metadata()[:request_id]` to *x-request-id* header.
+    """
 
-  @behaviour Tesla.Middleware
+    @behaviour Tesla.Middleware
 
-  require Logger
+    require Logger
 
-  @impl Tesla.Middleware
-  def call(%Tesla.Env{} = env, next, _opts) do
-    request_id = Logger.metadata()[:request_id]
+    @impl Tesla.Middleware
+    def call(%Tesla.Env{} = env, next, _opts) do
+      request_id = Logger.metadata()[:request_id]
 
-    if is_binary(request_id) do
-      env
-      |> Tesla.put_header("x-request-id", request_id)
-      |> Tesla.run(next)
-    else
-      Tesla.run(env, next)
+      if is_binary(request_id) do
+        env
+        |> Tesla.put_header("x-request-id", request_id)
+        |> Tesla.run(next)
+      else
+        Tesla.run(env, next)
+      end
     end
   end
 end
